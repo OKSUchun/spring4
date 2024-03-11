377427a9d68f3644d5ebd4ef4cf208130c677b05
OKSUchun <oksuchun119@gmail.com>
2024-03-11 17:01:52 +0900

    [fix] check TeacherPhoneNum duplicate

--- src/main/java/com/sparta/spring4/repository/TeacherRepository.java
+++ src/main/java/com/sparta/spring4/repository/TeacherRepository.java
@@ -3,5 +3,8 @@ package com.sparta.spring4.repository;
 import com.sparta.spring4.entity.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+    Optional<Teacher> findTeacherByPhoneNo(String phoneNo);
 }
